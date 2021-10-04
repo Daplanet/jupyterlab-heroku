cb5e6d987dc44b73810a34aeabd639245f49810e
Dwight Spencer <dspencer@computekindustries.com>
2021-10-04 02:53:13 -0500

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,37 +1,35 @@
 import os
 from pathlib import Path
-from invoke import exception, task, call
+from invoke import task, call
 
-extention_type_enum = [
+EXT_TYPE_ENUM = [
   "nb",
   "server",
   "lab"
 ]
 
-extentions = [
-  { name: "jupyter_dashboards", type: 0 },
-  { name: "jupyterlab", type: 1 },
-  { name: "voila", type: 1 },
-  { name: "@voila-dashboards/jupyterlab-preview", type: 2 }
-]
-
-root = Path("./root")
-
 @task
-def setup(ctx):
-  root.mkdir()
+def setup(ctx, props={}):
+  props['root'].mkdir()
 
-  for ext in extentions:
-    ctx.run(f"jupyter {extention_type_enum[ext.type]}extention install --py {ext.name} --sys-prefix")
-    ctx.run(f"jupyter {extention_type_enum[ext.type]}extention enable --py {ext.name} --sys-prefix")
+  for ext in props['extentions']:
+    ctx.run(f"jupyter {EXT_TYPE_ENUM[ext.type]}extention install --py {ext.name} --sys-prefix")
+    ctx.run(f"jupyter {EXT_TYPE_ENUM[ext.type]}extention enable --py {ext.name} --sys-prefix")
 
     if ext.type is 2:
       ctx.run(f"jupyter {extention_type_enum[ext.type]}extention install {ext.name}")      
 
 @task(default=True)
 def serve(ctx):
+  root = Path("./root")
+  exts = [
+    { name: "jupyter_dashboards", type: 0 },
+    { name: "jupyterlab", type: 1 },
+    { name: "voila", type: 1 },
+    { name: "@voila-dashboards/jupyterlab-preview", type: 2 }]
+
   if not root.is_dir():
-    call(setup)
+    call(setup, { root: root, extentions = exts })
 
   with ctx.cd(str(root)):
     with ctx.prefix("export JUPYTER_CONFIG_DIR=/app"):
