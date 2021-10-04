57c819ac869e139ed6a023c8e11061d1eb50fd31
Dwight Spencer <dspencer@computekindustries.com>
2021-10-04 02:09:57 -0500

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -4,23 +4,29 @@ from invoke import exception, task, call
 
 extention_type_enum = [
   "nb",
-  "server"
+  "server",
+  "lab"
 ]
 
 extentions = [
+  { name: "jupyter_dashboards", type: 0 },
   { name: "jupyterlab", type: 1 },
-  { name: "jupyter_dashboards", type: 0 }
+  { name: "voila", type 1 },
+  { name: "@voila-dashboards/jupyterlab-preview", type: 2 }
 ]
 
 root = Path("./root")
 
 @task
 def setup(ctx):
+  root.mkdir()
+
   for ext in extentions:
     ctx.run(f"jupyter {extention_type_enum[ext.type]}extention install --py {ext.name} --sys-prefix")
-    ctx.run(f"jupyter {extention_type_enum[ext.type]}extention enable --py {ext.nae} --sys-prefix")
+    ctx.run(f"jupyter {extention_type_enum[ext.type]}extention enable --py {ext.name} --sys-prefix")
 
-  root.mkdir()
+    if ext.type is 2:
+      ctx.run(f"jupyter {extention_type_enum[ext.type]}extention install {ext.name}")      
 
 @task(default=True)
 def serve(ctx):
