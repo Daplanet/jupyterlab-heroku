24ba4f441a9fecbd5c810ecfad2883fc5dffd12f
Dwight Spencer <dspencer@computekindustries.com>
2021-10-04 01:54:58 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 jupyter serverextension enable --py jupyterlab --sys-prefix
+jupyter nbextension install --py jupyter_dashboards --sys-prefix
+jupyter nbextension enable --py jupyter_dashboards --sys-prefix
+
 
 mkdir -p root
 cd root
